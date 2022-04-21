1dc2d857797a36f4594994a36fa54ce7e0870044
rofe <github@rofe.com>
2022-04-21 14:28:08 +0200

    feat: show update indicator on preview button

--- test/preview.test.js
+++ test/preview.test.js
@@ -129,4 +129,18 @@ describe('Test preview plugin', () => {
     assert.ok(!popupOpened, 'Unexpected popup opened');
     assert.ok(notification.className.includes('modal-config-success'), `Unexpected notification classes: ${notification.className}`);
   }).timeout(IT_DEFAULT_TIMEOUT);
+
+  it('Edit-specific preview plugin shows update indicator if edit is newer than preview', async () => {
+    const test = new SidekickTest({
+      url: 'https://adobe.sharepoint.com/:w:/r/sites/TheBlog/_layouts/15/Doc.aspx?sourcedoc=%7BE8EC80CB-24C3-4B95-B082-C51FD8BC8760%7D&file=bla.docx&action=default&mobileredirect=true',
+    });
+    const previewLastMod = test.apiResponses[0].preview.lastModified;
+    test.apiResponses[0].preview.lastModified = new Date(new Date(previewLastMod)
+      .setFullYear(2020)).toUTCString();
+    const { plugins } = await test.run();
+    assert.ok(
+      plugins.find((p) => p.id === 'edit-preview')?.classes.includes('update'),
+      'Preview plugin without update class',
+    );
+  }).timeout(IT_DEFAULT_TIMEOUT);
 });
