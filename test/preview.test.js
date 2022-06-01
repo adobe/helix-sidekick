a804edd3062a666734d433cee6a6d397b60ba29c
Dominique Pfister <dpfister@adobe.com>
2022-06-01 11:09:11 +0200

    Show `x-error` response header when config change fails (#308)
    * feat: use x-error if available
    
    * fix: show detail message
    
    * fix: move special handling to front
    
    * fix: check config change and error message
    
    * fix: show first part as well
    
    * chore: add test
    
    * Update src/sidekick/module.js
    
    Co-authored-by: Raphael Wegmueller <github@rofe.com>
    
    * Update src/sidekick/module.js
    
    Co-authored-by: Raphael Wegmueller <github@rofe.com>
    
    * chore: localized dialog message prefix
    
    * chore: remove only
    
    Co-authored-by: rofe <github@rofe.com>

--- test/preview.test.js
+++ test/preview.test.js
@@ -101,7 +101,7 @@ describe('Test preview plugin', () => {
 
   it('Edit-specific preview plugin updates preview when switching from editor', async () => {
     const { requestsMade } = await new SidekickTest({
-      url: 'https://adobe.sharepoint.com/:w:/r/sites/TheBlog/_layouts/15/Doc.aspx?sourcedoc=%7BE8EC80CB-24C3-4B95-B082-C51FD8BC8760%7D&file=bla.docx&action=default&mobileredirect=true',
+      url: 'https://adobe.sharepoint.com/:x:/r/sites/TheBlog/_layouts/15/Doc.aspx?sourcedoc=%7BE8EC80CB-24C3-4B95-B082-C51FD8BC8760%7D&file=bla.docx&action=default&mobileredirect=true',
       plugin: 'edit-preview',
     }).run();
     const updateReq = requestsMade
@@ -128,6 +128,25 @@ describe('Test preview plugin', () => {
     const { popupOpened, notification } = await test.run();
     assert.ok(!popupOpened, 'Unexpected popup opened');
     assert.ok(notification.className.includes('modal-config-success'), `Unexpected notification classes: ${notification.className}`);
+  }).timeout(IT_DEFAULT_TIMEOUT);
+
+  it('Edit-specific preview plugin shows /.helix/* error message from server', async () => {
+    const test = new SidekickTest({
+      url: 'https://adobe.sharepoint.com/:x:/r/sites/TheBlog/_layouts/15/Doc.aspx?sourcedoc=%7BE8EC80CB-24C3-4B95-B082-C51FD8BC8760%7D&file=test.xlsx&action=default&mobileredirect=true',
+      type: 'json',
+      plugin: 'edit-preview',
+    });
+    test.apiResponses[0].webPath = '/.helix/test.json';
+    test.apiResponses[1] = {
+      status: 502,
+      body: 'Bad Gateway',
+      headers: {
+        'x-error': 'foo',
+      },
+    };
+    const { popupOpened, notification } = await test.run();
+    assert.ok(!popupOpened, 'Unexpected popup opened');
+    assert.strictEqual(notification.message, 'foo', `Unexpected notification message: ${notification.message}`);
   }).timeout(IT_DEFAULT_TIMEOUT);
 
   it('Edit-specific preview plugin shows update indicator if edit is newer than preview', async () => {
